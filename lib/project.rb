016f2f22bdfe5657b8725c463b91460545fd286e
Warren Harper II <warrenharper2@att.net>
2019-08-27 20:54:21 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,14 +1,14 @@
 class Project
-  attr_accessor :title, :backers
+  attr_reader :title, :backers
 
   def initialize(title)
     @title = title
     @backers = []
   end
 
-  def add_backer(backers)
-    @backers << backers
-    backers.back_project(self) unless backers.backed_projects.include?(self)
+  def add_backer(backer)
+    @backers << backer
+    backers.back_project(self) << self
   end
 end
 title = title.new("Project With So Much Amaze")
